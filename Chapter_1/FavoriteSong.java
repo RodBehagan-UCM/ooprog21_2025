453ff2ff6ec10bc47ef28790d9692bc9f889fe1c
Rod Behagan <Rod Behagan>
2025-08-29 20:48:16 +0800

    Signed-off-by: Rod Behagan <Rod Behagan>

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Kapag makapiling ka");
+        System.out.println("Hindi alam ang gagawin, iiwas ba o titingin");
+        System.out.println("Sa 'yong kagandahan?");
+        System.out.println("Ang kislap ng iyong mata ay 'di ko na nakikita");
+    }
+}
