ec00f200089b8c4b0513fb35016692f142ae1045
Rod Behagan <Rod Behagan>
2025-08-29 20:54:39 +0800

    Signed-off-by: Rod Behagan <Rod Behagan>

---
+++ Chapter_1/FirstDialog.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+ public class FirstDialog
+ {
+ 
+ public static void main (String [] args)
+ {
+ 
+   JOptionPane.showMessageDialog(null, "First java dialog"," Message",JOptionPane.INFORMATION_MESSAGE );
+   
+ }
+ }
+ 
+ 
