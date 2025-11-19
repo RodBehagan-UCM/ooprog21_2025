f2ee62ebf9dd20c4e7f315f30ea87929cce4596d
RodBehagan-UCM <rodadrianbehagan@gmail.com>
2025-11-19 22:44:45 +0800

    Add files via upload

---
+++ Chapter_7/DebugSeven2.java
@@ -0,0 +1,50 @@
+// Program prompts user to enter a series of integers
+// separated by spaces
+// Program converts them to numbers and sums them
+import java.util.*;
+public class DebugSeven2
+{
+   public static void main(String[] args)
+   {
+      String str;
+      int x;
+      int length;
+      int start = 0;
+      int num;
+      int lastSpace = -1;
+      int sum = 0;
+      String partStr;
+      Scanner in = new Scanner(System.in);
+      System.out.print("Enter a series of integers separated by spaces >> ");
+      str = in.nextLine();
+
+      // fixed str.len() to str.length()
+      length = str.length();
+
+      // fixed str.length to str.length()
+      for(x = 0; x < length; ++x)
+      {
+         if(str.charAt(x) == ' ')
+         {
+             partStr = str.substring(lastSpace + 1, x);     
+             num = Integer.parseInt(partStr);
+             System.out.println("                " + num);
+             sum += num;
+             lastSpace = x;
+         } 
+      }
+
+      // used correct variable names
+      partStr = str.substring(lastSpace + 1, length);
+
+      // fixed parStr to partStr
+      num = Integer.parseInt(partStr);
+
+      System.out.println("                " + num);
+
+      sum += num; //fixed = to +=
+
+      System.out.println("         -------------------" +
+         "\nThe sum of the integers is " + sum);
+   }
+}
